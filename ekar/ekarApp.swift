c04e10e91b1f4ecfc599a76e9a9b5f313591ce18
Alan Steiman <asteiman@gmail.com>
2021-07-16 12:16:07 +0800

    Initial commit

---
+++ ekar/ekarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ekarApp.swift
+//  ekar
+//
+//  Created by Alan Steiman on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ekarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
