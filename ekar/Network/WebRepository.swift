5465173acabfa86f4cf42b0b21c261d1ed6c638e
Alan Steiman <asteiman@gmail.com>
2021-07-25 17:41:04 +0800

    Remove unused code

--- ekar/Network/WebRepository.swift
+++ ekar/Network/WebRepository.swift
@@ -57,21 +57,3 @@ private extension Publisher where Output == URLSession.DataTaskPublisher.Output 
         .eraseToAnyPublisher()
     }
 }
-
-private extension Publisher {
-    /// Holds the downstream delivery of output until the specified time interval passed after the subscription
-    /// Does not hold the output if it arrives later than the time threshold
-    ///
-    /// - Parameters:
-    ///   - interval: The minimum time interval that should elapse after the subscription.
-    /// - Returns: A publisher that optionally delays delivery of elements to the downstream receiver.
-
-    func ensureTimeSpan(_ interval: TimeInterval) -> AnyPublisher<Output, Failure> {
-        let timer = Just<Void>(())
-            .delay(for: .seconds(interval), scheduler: RunLoop.main)
-            .setFailureType(to: Failure.self)
-        return zip(timer)
-            .map { $0.0 }
-            .eraseToAnyPublisher()
-    }
-}
